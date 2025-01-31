90605a40435c4693af0649f10e7750a2b6734125
Tudor Popescu <tudor.m.p@outlook.com>
2025-02-01 00:17:18 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,152 @@
+const submitButton = document.getElementById('submit-button');
+const answersSection = document.getElementById('answers-section');
+const tutorialSection = document.getElementById('tutorial-section');
+const resultsSection = document.getElementById('results-section');
+const heading = document.getElementById('heading');
+let questionCount = 0;
+let data = {};
+let questionObject = {};
+let score = 0;
+let correctAnswerNum = null;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Handle click on the submitButton
+    submitButton.addEventListener('click', () => {
+        if (submitButton.classList.contains('disabled')) {
+            return;
+        }
+        if (questionCount === 0 || submitButton.innerText === 'Start') {
+            startQuiz();
+        } else if (questionCount === 10) {
+            finishQuiz();
+        } else if (questionCount === 11) {
+            restartQuiz();
+        } else {
+            nextQuestion();
+        }
+    });
+
+    // Handle click on the answer options
+    for (let i = 1; i <= 4; i++) {
+        const selectedAnswer = document.getElementById(`answer-${i}`);
+        selectedAnswer.addEventListener('click', () => {
+            // Check that button is not disabled (which is the case when an answer was already selected)
+            if (!selectedAnswer.classList.contains('disabled')) {
+                if (i === correctAnswerNum) {
+                    selectedAnswer.classList.add('correct');
+                    score += 1;
+                } else {
+                    selectedAnswer.classList.add('incorrect');
+                    document.getElementById(`answer-${correctAnswerNum}`).classList.add('correct');
+                }
+                // Deactivate all other answer buttons
+                deactivateOtherAnswers(i);
+                // Activate submitButton to navigate to the next question
+                submitButton.classList.remove('disabled');
+            }
+        });
+    }
+});
+
+const fetchQuestionsFromTriviaAPI = async () => {
+    try {
+        const result = await fetch('https://opentdb.com/api.php?amount=10&category=12&type=multiple');
+        const json = await result.json();
+        return json;
+    } catch (error) {
+        // Handle failing to fetch data
+    }
+}
+
+const startQuiz = async () => {
+    // Fetch questions from Trivia API
+    data = await fetchQuestionsFromTriviaAPI();
+    submitButton.innerText = 'Next';
+    // Hide the tutorial
+    tutorialSection.classList.add('hidden');
+    // Show the first question
+    answersSection.classList.remove('hidden');
+    // Populate question
+    nextQuestion();
+}
+
+const finishQuiz = () => {
+    // Update heading to results
+    heading.innerText = 'Results';
+    // Hide answers section, display results section
+    answersSection.classList.add('hidden');
+    resultsSection.classList.remove('hidden');
+    // Display score
+    document.getElementById('score').innerText = score + ` point${score > 1 ? 's' : ''}`;
+    // Update submit button
+    submitButton.innerText = 'Restart quiz';
+    // Increase question count
+    questionCount += 1;
+}
+
+const nextQuestion = () => {
+    // Get question object from data
+    const questionObject = data.results[questionCount];
+    questionCount += 1;
+
+    // Update heading with question text (function learnt from chatGPT)
+    heading.innerText = questionCount + '. ' + decodeHtmlEntities(questionObject.question);
+
+    // Populate answers, randomising the placement of the correct answer
+    correctAnswerNum = Math.floor(Math.random() * 4) + 1;
+    let incorrectAnswers = questionObject['incorrect_answers'];
+    for (let i = 1; i <= 4; i++) {
+        const answerButton = document.getElementById(`answer-${i}`);
+        // Reset answer button status
+        answerButton.classList.remove('correct', 'incorrect');
+        if ( i === correctAnswerNum ) {
+            answerButton.innerText = decodeHtmlEntities(questionObject['correct_answer']);
+        } else {
+            answerButton.innerText = decodeHtmlEntities(incorrectAnswers.shift());
+        }
+    }
+    // Re-enable all answer buttons
+    activateAllAnswerButtons();
+    // Disable submit button until user selects an answer
+    submitButton.classList.add('disabled');
+    // Change submit button to finish on last question
+    if (questionCount === 10) {
+        submitButton.innerText = 'Finish';
+    }
+}
+
+const restartQuiz = () => {
+    // Reset question count and score
+    questionCount = 0;
+    score = 0;
+    // Populate content to display the tutorial
+    submitButton.innerText = 'Start';
+    heading.innerText = 'How to play';
+    resultsSection.classList.add('hidden');
+    tutorialSection.classList.remove('hidden');
+}
+
+
+// UTILITIES
+
+// Decode html entities present in data (function learnt from chatGPT)
+const decodeHtmlEntities = (str) => {
+    const parser = new DOMParser();
+    const decoded = parser.parseFromString(str, 'text/html').body.textContent;
+    return decoded;
+}
+
+const deactivateOtherAnswers = (selectedAnswerIdx) => {
+    for (let i = 1; i <= 4; i++) {
+        if (i !== selectedAnswerIdx) {
+            document.getElementById(`answer-${i}`).classList.add('disabled');
+        }
+    }
+}
+
+const activateAllAnswerButtons = () => {
+    for (let i = 1; i <= 4; i++) {
+        document.getElementById(`answer-${i}`).classList.remove('disabled');
+    }
+}
